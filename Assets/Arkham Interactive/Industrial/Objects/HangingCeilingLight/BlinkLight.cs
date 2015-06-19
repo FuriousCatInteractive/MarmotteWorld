ce78f41de377b5c43d08048f2748d4d8d4907090
Steven GERARD <steven.gerard.deruelle@gmail.com>
2015-06-19 12:56:03 -0400

    Salle 1 fini
    Former-commit-id: dd43fba4eb33aaf1548578a24671741a202f614f

--- Assets/Arkham Interactive/Industrial/Objects/HangingCeilingLight/BlinkLight.cs
+++ Assets/Arkham Interactive/Industrial/Objects/HangingCeilingLight/BlinkLight.cs
@@ -2,11 +2,23 @@
 using System.Collections;
 
 public class BlinkLight : MonoBehaviour {
-
+    public float minRange;
+    public float maxRange;
+    private float timer;
 	// Use this for initialization
 	void Start () {
-	
+        StartCoroutine(Blink());
 	}
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            timer = Random.Range(minRange, maxRange);
+            yield return new WaitForSeconds(timer);
+            GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
