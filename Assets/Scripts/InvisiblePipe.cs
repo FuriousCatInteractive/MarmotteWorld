700dd2bef5adbea88186760102a2f0fe4ea8eafc
Steven GERARD <steven.gerard.deruelle@gmail.com>
2015-06-20 13:08:39 -0400

    Placement des tuyaux ok
    Former-commit-id: e94a5e573a642bd1d6e77b65f785ab14c28a0c74

---
+++ Assets/Scripts/InvisiblePipe.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvisiblePipe : MonoBehaviour {
+    public int numerator;
+    public int denominator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("pipe") && numerator == other.GetComponent<Pipe>().numerator && denominator == other.GetComponent<Pipe>().denominator)
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+            GameObject.Destroy(other.gameObject);
+        }
+        else
+        {
+            print("Test pipe");
+        }
+    }
+}
---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe : MonoBehaviour {
+    public int numerator;
+    public int denominator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
