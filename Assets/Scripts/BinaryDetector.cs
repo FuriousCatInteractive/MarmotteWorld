8410fded5a2893b34b927f436a36310e6e33d223
Steven GERARD <steven.gerard.deruelle@gmail.com>
2015-06-20 11:50:02 -0400

    Ajout gameplay dernière enigme
    Former-commit-id: cf39766327621c1bdeb5cc7e3263e2f3790c2672

---
+++ Assets/Scripts/BinaryDetector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BinaryDetector : MonoBehaviour {
+    public bool activate = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("binaryCube")) 
+        {
+            activate = true;
+        }
+        else
+        {
+            print("Test");
+        }
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        if (true)
+        {
+            activate = true;
+        }
+        else
+        {
+            print("Test");
+        }
+          
+    }
+}
---
+++ Assets/Scripts/BinaryDoor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BinaryDoor : MonoBehaviour {
+    public GameObject[] solve;
+    private Animation anim;
+    private bool isOpen = false;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool tmp = true;
+        for (int i = 0; i < solve.Length; ++i)
+        {
+            var test = solve[i].GetComponent<BinaryDetector>().activate;
+            if (!solve[i].GetComponent<BinaryDetector>().activate)
+            {
+                tmp = false;
+            }
+        }
+        if (tmp && !isOpen)
+        {
+            anim.Play("open");
+            isOpen = true;
+        }
+	}
+}
