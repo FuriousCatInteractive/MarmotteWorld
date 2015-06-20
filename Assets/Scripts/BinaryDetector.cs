cefbba52a10ad4ffc6ec24af9dc85213a21a434a
Steven GERARD <steven.gerard.deruelle@gmail.com>
2015-06-20 17:43:49 -0400

    Début seau
    Former-commit-id: d1f266f4b5ace96d774c799c82621c03597c4b95

--- Assets/Scripts/BinaryDetector.cs
+++ Assets/Scripts/BinaryDetector.cs
@@ -35,7 +35,6 @@ public class BinaryDetector : MonoBehaviour {
         else
         {
             print("Test");
-        }
-          
+        }  
     }
 }
---
+++ Assets/Scripts/Bucket.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bucket : MonoBehaviour {
+    public bool isFull = false;
+    public int size = 1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void FillBucket()
+    {
+        isFull = true;
+        var tmp = gameObject.GetComponentInChildren<UnityStandardAssets.Water.Water>();
+        tmp.enabled = true;
+        tmp.GetComponentInParent<MeshRenderer>().enabled = true;
+    }
+}
