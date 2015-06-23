faf3875abf2bc336c83ebae9cd62d371fcf45954
corentin-R <corentinraoult@laposte.net>
2015-06-23 15:38:45 -0400

    salle 1 finie avec marmotte UI
    Former-commit-id: 2676982f82023c2e373f8714c2abf9389cbd2cd0

--- Assets/marmotteSpeak.cs
+++ Assets/marmotteSpeak.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class marmotteSpeak : MonoBehaviour {
 
     //temps d apparition du texte a l ecran
-    public float timeShown = 5.0F;
+    public float timeShown = 10.0F;
 
     private Canvas canvas;
     private float cpt;
@@ -14,7 +14,7 @@ public class marmotteSpeak : MonoBehaviour {
 	void Start () {
         cpt = 0.0F;
         canvas = GameObject.FindGameObjectWithTag("marmotteUI").GetComponent<Canvas>();
-        canvas.enabled = false;
+        //canvas.enabled = false;
  	}
 	
 	// Update is called once per frame
