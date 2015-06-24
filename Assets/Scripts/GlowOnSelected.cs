0864c9d0de8198270d5a334a15a118db89e1fe8c
Taiki Hagiwara <taiki.hagiwara@gmail.com>
2015-06-23 20:24:34 -0400

    glow script
    Former-commit-id: afe149320a27fbdc5cd926e02ab0e355eb0ee5f7

---
+++ Assets/Scripts/GlowOnSelected.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowOnSelected : MonoBehaviour {
+
+    public Material glowMaterial;
+
+    private Transform m_Transform;
+    private Mesh m_Mesh;
+    private bool m_Glow = false;
+
+    void Awake()
+    {
+        m_Transform = transform;
+        m_Mesh = GetComponent<MeshFilter>().sharedMesh;
+    }
+
+    void MyPostRender(Camera camera)
+    {
+        glowMaterial.SetPass(0);
+
+        Graphics.DrawMeshNow(m_Mesh, m_Transform.localToWorldMatrix);
+    }
+
+    public void ToggleGlow()
+    {
+        m_Glow = !m_Glow;
+        if (m_Glow)
+            Camera.onPostRender += MyPostRender;
+        else
+            Camera.onPostRender -= MyPostRender;
+    }
+
+}
