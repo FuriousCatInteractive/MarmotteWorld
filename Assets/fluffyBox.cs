a952ef9855899766c01e1ed2f58299ecc174fded
corentin-R <corentinraoult@laposte.net>
2015-06-24 22:14:54 -0400

    debut fluffybox
    Former-commit-id: 5991dd499da9828d64bca070e8215318f232e968

---
+++ Assets/fluffyBox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fluffyBox : MonoBehaviour {
+
+    public string text;
+
+	// Use this for initialization
+	void Start () {
+       // text = "caca";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.CompareTag("Player"))
+        {
+            GameObject.FindGameObjectWithTag("marmotteUI").GetComponent<marmotteSpeak>().marmotteSays(text, 10.0F);            
+           
+        }
+    }
+
+
+}
+    
